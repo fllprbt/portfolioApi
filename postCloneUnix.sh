b3b5bbed736d5c8ebc81ca8f23b1ec612d5147fb
fllprbt <pgiouvanelis@gmail.com>
2018-11-30 02:52:14 +0100

    Fix missing string on script's comments

--- postCloneUnix.sh
+++ postCloneUnix.sh
@@ -26,5 +26,5 @@ docker-compose exec app php artisan migrate
 # add test user (mail: test@user.com, pass: test)
 docker-compose exec app php artisan db:seed
 
-# open output of containers and ensure they will be terminated on si
+# open output of containers and ensure they will be terminated on SIGINT
 docker-compose logs -f || docker-compose down
