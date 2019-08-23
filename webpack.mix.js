a3dfc1826f777ef9602fe3fca8fa053cccc18320
fllprbt <pgiouvanelis@gmail.com>
2019-08-23 14:01:32 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,5 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.ts('resources/js/app.tsx', 'public/js')
     .options({
         hmrOptions: { host: '0.0.0.0', port: 8088 },
