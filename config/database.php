4fbabdb0fb3dda8a5961d61c9cf79e13e8d92e14
fllprbt <palaiologos.giouvanelis@leaseplan.com>
2020-03-20 21:15:40 +0100

    change db conf

--- config/database.php
+++ config/database.php
@@ -35,10 +35,16 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => storage_path('database.sqlite'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+
+        'sqlite_testing' => [
+			'driver'   => 'sqlite',
+			'database' => storage_path().'/testing.sqlite',
+			'prefix'   => '',
+		],
 
         'mysql' => [
             'driver' => 'mysql',
